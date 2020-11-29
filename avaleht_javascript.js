659c728993d4d84f76f3f8d6ea57b06e7b40d0bf
raikim2nd <raiki.mnd@gmail.com>
2020-11-29 15:25:13 +0200

    avalehe javascript

---
+++ avaleht_javascript.js
@@ -0,0 +1,15 @@
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/2022_FIFA_World_Cup.svg/800px-2022_FIFA_World_Cup.svg.png","https://upload.wikimedia.org/wikipedia/en/thumb/9/96/UEFA_Euro_2020_Logo.svg/330px-UEFA_Euro_2020_Logo.svg.png","https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/UEFA_Champions_League_logo_2.svg/179px-UEFA_Champions_League_logo_2.svg.png");

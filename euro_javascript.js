28692decd607f7c4c98c904a32dba24cab7c34b9
raikim2nd <73638651+raikim2nd@users.noreply.github.com>
2020-11-30 21:16:52 +0200

    euro javascripti uuendus

--- euro_javascript.js
+++ euro_javascript.js
@@ -12,4 +12,4 @@ function preloadImages(){
 
 }
 
-preloadImages("https://cdni0.trtworld.com/w960/h540/q75/1976-trtworld-141171-173026.jpg", "https://upload.wikimedia.org/wikipedia/en/thumb/9/96/UEFA_Euro_2020_Logo.svg/1200px-UEFA_Euro_2020_Logo.svg.png");
+preloadImages("https://cdni0.trtworld.com/w960/h540/q75/1976-trtworld-141171-173026.jpg", "https://upload.wikimedia.org/wikipedia/en/thumb/9/96/UEFA_Euro_2020_Logo.svg/1200px-UEFA_Euro_2020_Logo.svg.png", "https://images.daznservices.com/di/library/GOAL/93/4f/cristiano-ronaldo-euro-2016-european-championships-france_14iatl8tqnc7fzm7tijr676m7.jpg?t=-624887019&quality=100");

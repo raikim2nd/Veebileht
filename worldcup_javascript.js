00459c77a19f931d78905bc57957a7839064ecfa
raikim2nd <73638651+raikim2nd@users.noreply.github.com>
2020-11-30 21:21:31 +0200

    worldcupi javascripti uuendus

--- worldcup_javascript.js
+++ worldcup_javascript.js
@@ -12,4 +12,4 @@ function preloadImages(){
 
 }
 
-preloadImages("https://assets.stickpng.com/images/58430032a6515b1e0ad75b3f.png", "https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/2022_FIFA_World_Cup.svg/800px-2022_FIFA_World_Cup.svg.png");
+preloadImages("https://cdni0.trtworld.com/w960/h540/q75/34736_worldcup_1528293488717.jpg", "https://img.fifa.com/image/upload/t_l1/em4mlli1thcvngvouceb.jpg", "https://assets.stickpng.com/images/58430032a6515b1e0ad75b3f.png", "https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/07/15/18/2018-07-15T173311Z-387054244-UP1EE7F1CRABL-RTRMADP-3-SOCCER-FINAL.JPG?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5", "https://images.indianexpress.com/2018/07/luka-modric-fb.jpg", "https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/2022_FIFA_World_Cup.svg/800px-2022_FIFA_World_Cup.svg.png");
